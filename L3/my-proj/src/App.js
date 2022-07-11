f68998838dab7cc874ee24de9e50432397149fe1
satya <satyaranjanrayrazzor@gmail.com>
2022-07-11 22:31:59 +0530

    "UseState.1[# only Change in varriable wont reflect the change"

--- L3/my-proj/src/App.js
+++ L3/my-proj/src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name='Satya';
+  const handleClick=()=>{
+    name='Roy';
+    console.log(name);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome {name}</h1>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
