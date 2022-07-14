3ba905e9565922cf3b71dcce998dd09f40f72cbc
satya <satyaranjanrayrazzor@gmail.com>
2022-07-14 16:52:05 +0530

    "UseState[#Outputing list]"

--- L3/my-proj/src/App.js
+++ L3/my-proj/src/App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
 function App() {
-   const [name,setName]= useState('Satya');
-  const handleClick=()=>{
-    setName('Roy');
-    console.log(name);
-  }
-  const [list,setList]=useState([
+  const [name,setName]= useState('Satya');
+
+  const [events,setEvents]=useState([
     {title: 'Valorant' , id: 1},
     {title: 'Dota2' , id: 2},
     {title: 'CS-GO' , id: 3},
   ]
   );
+  
 
   return (
     <div className="App">
       <h1>Welcome {name}</h1>
-
-        {list.map((things)=>(
-          <p>{things.title}</p>
+        {events.map((event)=>(
+          <div>
+            <h2>{event.title}</h2>
+          </div>
         ))}
-
     </div>
   );
 }
